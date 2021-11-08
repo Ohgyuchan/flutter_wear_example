70785c365716d801286de49ce7751edeed4888d7
Ohgyuchan <gyu3637@gmail.com>
2021-11-08 21:56:54 +0900

    Update flutter_watch_example

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:date_format/date_format.dart';
 import 'package:flutter/material.dart';
+import 'package:timer_builder/timer_builder.dart';
 import 'package:wear/wear.dart';
 
 void main() => runApp(MyApp());
@@ -10,27 +12,98 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         body: Center(
           child: WatchShape(
-            builder: (context, shape, child) {
-              return Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Text(
-                    'Shape: ${shape == WearShape.round ? 'round' : 'square'}',
+            child: ListView(
+              children: [
+                Container(
+                  color: Colors.blue,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Expanded(
+                        child: Text(
+                          "Play Routine",
+                        ),
+                      ),
+                      _nowTime(),
+                      Row(
+                        children: [
+                          Expanded(
+                            child: Text(
+                              "실시간 인기 루틴",
+                            ),
+                          ),
+                          MaterialButton(
+                            onPressed: () {
+                              print('click!');
+                            },
+                            child: Row(
+                              children: [
+                                Text(
+                                  "더보기",
+                                ),
+                                Icon(
+                                  Icons.forward,
+                                )
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                      Center(
+                        child: Text(
+                          "아직 생성된 루틴이 없습니다.",
+                        ),
+                      ),
+                      Row(
+                        children: [
+                          Expanded(
+                            child: Text(
+                              "당신을 위한 추천 루틴",
+                            ),
+                          ),
+                          MaterialButton(
+                            onPressed: () {
+                              print('click');
+                            },
+                            child: Row(
+                              children: [
+                                Text(
+                                  "더보기",
+                                ),
+                                Icon(
+                                  Icons.forward,
+                                )
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                      Center(
+                        child: Text(
+                          "아직 생성된 루틴이 없습니다.",
+                        ),
+                      ),
+                    ],
                   ),
-                  child,
-                ],
-              );
-            },
-            child: AmbientMode(
-              builder: (context, mode, child) {
-                return Text(
-                  'Mode: ${mode == WearMode.active ? 'Active' : 'Ambient'}',
-                );
-              },
+                ),
+              ],
             ),
           ),
         ),
       ),
+    );
+  }
+
+  Widget _nowTime() {
+    return TimerBuilder.periodic(
+      const Duration(minutes: 1),
+      builder: (context) {
+        return Text(
+          formatDate(DateTime.now(),
+              [mm, '월 ', dd, '일  ', hh, '시 ', nn, '분  ', D, '요일'],
+              locale: KoreanDateLocale()),
+        );
+      },
     );
   }
 }
